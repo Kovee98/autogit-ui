6a659913a717a228d979caa962bb4cba2d1077cf
kovee98 <kovee98@gmail.com>
2021-11-27 08:34:16 -0600

    improve notecard

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -14,54 +14,55 @@
     <!-- backdrop -->
     <div
         v-if="isOpen"
-        @click="closeNote"
-        class="absolute left-0 top-0 w-screen h-screen bg-black opacity-75 z-index-100 flex items-center justify-center"
-    />
-    <div
-        v-if="isOpen"
-        class="absolute left-20 right-20 top-25 bottom-25 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 z-index-200 flex flex-col"
+        class="absolute left-0 top-0 w-screen h-screen z-index-100 flex items-center justify-center"
     >
-        <div class="w-full flex justify-between">
-            <input
-                type="text"
-                v-model="form.title"
-                class="bg-transparent text-2xl font-semibold text-gray-700 dark:text-gray-200 w-full focus:outline-none border-b border-gray-700 p-2 mb-3"
-            >
-
-            <i
-                @click="closeNote"
-                class="icon-cancel p-1 cursor-pointer text-lg text-gray-500 mb-6"
-            />
-        </div>
-
-        <ToastEditor
-            v-model="form.body"
+        <div
+            @click="closeNote"
+            class="absolute left-0 top-0 w-screen h-screen bg-black opacity-75 z-40"
         />
+        <div class="w-140 h-225 z-50 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 z-index-200 flex flex-col">
+            <div class="w-full flex justify-between">
+                <input
+                    type="text"
+                    v-model="form.title"
+                    class="bg-transparent text-2xl font-semibold text-gray-700 dark:text-gray-200 w-full focus:outline-none border-b border-gray-700 p-2 mb-3"
+                >
 
-        <!-- actions -->
-        <div class="w-full justify-between flex border-t border-gray-700 pt-3">
-            <button
-                @click="deleteNote"
-                class="btn-danger mr-2"
-            >
-                <i class="icon-trash cursor-pointer text-lg text-gray-200" />
-                <!-- Delete -->
-            </button>
-
-            <div class="float-right">
-                <button
+                <i
                     @click="closeNote"
-                    class="btn-flat mr-2"
+                    class="icon-cancel p-1 cursor-pointer text-lg text-gray-500 mb-6"
+                />
+            </div>
+
+            <ToastEditor
+                v-model="form.body"
+            />
+
+            <!-- actions -->
+            <div class="w-full justify-between flex border-t border-gray-700 pt-3">
+                <button
+                    @click="deleteNote"
+                    class="btn-danger mr-2"
                 >
-                    Cancel
+                    <i class="icon-trash cursor-pointer text-lg text-gray-200" />
+                    <!-- Delete -->
                 </button>
 
-                <button
-                    @click="saveNote"
-                    class="btn-success"
-                >
-                    Save
-                </button>
+                <div class="float-right">
+                    <button
+                        @click="closeNote"
+                        class="btn-flat mr-2"
+                    >
+                        Cancel
+                    </button>
+
+                    <button
+                        @click="saveNote"
+                        class="btn-success"
+                    >
+                        Save
+                    </button>
+                </div>
             </div>
         </div>
     </div>
