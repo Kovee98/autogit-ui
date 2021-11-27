2496906bbacbf4f0c14c514a7b33c1b1db1f72da
kovee98 <kovee98@gmail.com>
2021-11-27 08:25:12 -0600

    improve fab

--- src/components/PlusButton.vue
+++ src/components/PlusButton.vue
@@ -1,18 +1,56 @@
 <template>
-    <button
-        @click="addNote"
-        class="fixed rounded-full bottom-8 right-8 bg-yellow-800 w-15 h-15 shadow-md"
-    >
-        <i class="icon-plus text-gray-200 text-lg" />
-    </button>
+    <div class="fixed bottom-8 right-8">
+        <div class="flex flex-col items-center justify-center">
+            <div
+                class="relative flex items-center transition-all"
+                :class="isOpen ? 'bottom-[3rem]' : '-bottom-24'"
+            >
+                <div
+                    class="fixed right-[7rem] p-3 bg-black rounded-md text-gray-200 transition-all delay-100"
+                    :class="isOpen ? 'opacity-100' : 'opacity-0'"
+                >
+                    Add Collection
+                </div>
+                <button class="rounded-full bg-yellow-800 w-12 h-12 shadow-md z-40">
+                    <i class="icon-plus text-gray-200 text-lg" />
+                </button>
+            </div>
+            <div
+                class="relative flex items-center transition-all"
+                :class="isOpen ? 'bottom-[1.5rem]' : '-bottom-12'"
+            >
+                <div
+                    class="fixed right-[7rem] p-3 bg-black rounded-md text-gray-200 transition-all delay-100"
+                    :class="isOpen ? 'opacity-100' : 'opacity-0'"
+                >
+                    Add Note
+                </div>
+                <button
+                    @click="addNote"
+                    class="relative rounded-full bg-yellow-800 w-12 h-12 shadow-md z-40"
+                >
+                    <i class="icon-plus text-gray-200 text-lg" />
+                </button>
+            </div>
+            <button
+                @click="isOpen = !isOpen"
+                class="relative rounded-full bg-yellow-800 w-18 h-18 shadow-md z-50 transition-all"
+                :class="isOpen ? 'rotated' : ''"
+            >
+                <i class="icon-plus text-gray-200 text-2xl" />
+            </button>
+        </div>
+    </div>
 </template>
 
 <script>
+    import { ref } from 'vue';
     import db from '../js/rxdb.js';
     import emitter from '../js/mitt.js';
 
     export default {
         setup () {
+            const isOpen = ref(false);
             const addNote = async () => {
                 const res = await db?.notes?.put({
                     title: 'Untitled Note',
@@ -20,12 +58,21 @@
                     tags: []
                 });
 
+                isOpen.value = false;
+
                 emitter.emit('open-note', res.id);
             };
 
             return {
+                isOpen,
                 addNote,
             };
         }
     }
 </script>
+
+<style lang="scss" scoped>
+    .rotated {
+        transform: rotate(45deg);
+    }
+</style>
